83443d45738c5f0d7d4a7d797a70a57d0cdcb1f4
thenightzhou <thenightzhou@163.com>
2026-08-15 13:07:58 +0800

    文件分类

---
+++ UploadProcess.py
@@ -0,0 +1,9 @@
+# 保存文件并上传github流程
+# 1.修改完成后按Ctrl + S 保存
+# 2.在终端输入 "git add '文件名' " 
+#   例如 "git add hello.py "
+#   添加文件夹内所有文件 "git add . "
+# 3.在终端输入 "git commit -m "修改内容" "
+#   例如 "git commit -m "增加了某某功能" "
+# 4.在终端输入 "git push "
+# 网页端按f5刷新页面
--- hello.py
+++ hello.py
@@ -1,17 +1,2 @@
 print("My first code")
 print("Hello World")
-
-
-
-
-
-
-
-# 工作流程
-# 1.修改完成后按Ctrl + S 保存
-# 2.在终端输入 "git add '文件名' " 
-#             "git add hello.py "
-# 添加文件夹内所有文件"git add . "
-# 3.在终端输入 "git commit -m "修改内容" "
-#             "git commit -m "增加了某某功能" "
-# 4.在终端输入 "git push "
