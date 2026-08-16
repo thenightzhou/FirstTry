fa611441c4fad11f1223d06110d80b6615587786
thenightzhou <thenighzhou@163.com>
2026-08-16 15:17:28 +0800

    丰富了操作流程

--- UploadProcess.py
+++ UploadProcess.py
@@ -7,3 +7,12 @@
 #   例如 "git commit -m "增加了某某功能" "
 # 4.在终端输入 "git push "
 # 网页端按f5刷新页面
+
+# 将你的新项目关联至github
+# 1.登录github创建 New repository
+# 2.在终端输入"git config --global user.name"
+# 3.在终端输入"git config --global user.email"
+# 4.在终端输入"git init"
+# 5.完成第一大块内容
+# 6.在终端输入"git remote add origin https://github.com/用户名/仓库名.git"
+# 7.在终端输入"git push -u origin main"
